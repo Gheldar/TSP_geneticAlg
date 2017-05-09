398cbc48204201405aa917a1aff0fe4060e4b08d
Gheldar <gorgo_albertoni@yahoo.gr>
2017-05-09 23:06:27 +0200

    Initial upload

---
+++ TSP_geneticAlg/Path.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP_geneticAlg
+{
+    class Path
+    {
+        public int[] selection = new int[5];
+        public int length = 0;
+
+        public Path()
+        {
+            // Fill path array with unique to each other cities
+            for (int i = 0; i < 5; i++)
+            {
+                Random random = new Random();
+                selection[i] = random.Next(0, 5);
+                
+                for (int j = 0; j < 5; j++)
+                {
+                    if (selection[i] == selection[j] && i != j)
+                    {
+                        i--;
+                        j = 5;
+                    }
+                }
+            }
+
+            length = CalculateDistance();
+        }
+
+
+        public Path(int i0, int i1, int i2, int i3, int i4)
+        {
+            selection[0] = i0;
+            selection[1] = i1;
+            selection[2] = i2;
+            selection[3] = i3;
+            selection[4] = i4;
+
+            length = CalculateDistance();
+        }
+
+
+        // Find the path's distance (fitness)
+        public int CalculateDistance()
+        {
+            int[,] distances = new int[5, 5];
+            // A (0)
+            distances[0, 0] = 0;
+            distances[0, 1] = 4;
+            distances[0, 2] = 4;
+            distances[0, 3] = 7;
+            distances[0, 4] = 3;
+            // B (1)
+            distances[1, 0] = 4;
+            distances[1, 1] = 0;
+            distances[1, 2] = 2;
+            distances[1, 3] = 3;
+            distances[1, 4] = 5;
+            // C (2)
+            distances[2, 0] = 4;
+            distances[2, 1] = 2;
+            distances[2, 2] = 0;
+            distances[2, 3] = 2;
+            distances[2, 4] = 3;
+            // D (3)
+            distances[3, 0] = 7;
+            distances[3, 1] = 3;
+            distances[3, 2] = 2;
+            distances[3, 3] = 0;
+            distances[3, 4] = 6;
+            // E (4)
+            distances[4, 0] = 3;
+            distances[4, 1] = 5;
+            distances[4, 2] = 3;
+            distances[4, 3] = 6;
+            distances[4, 4] = 0;
+
+            bool eliminateLength = false;
+            for(int i = 0; i < 5; i++) // Check if all cities are unique
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if (i != j)
+                    {
+                        if (selection[i] == selection[j])
+                            eliminateLength = true;
+                    }
+                }
+            }
+
+            if (!eliminateLength)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    int currentCity = selection[i];
+                    int nextCity;
+                    if (i == 4)
+                        nextCity = selection[0]; // if we are at the last city, next city is the first one
+                    else
+                        nextCity = selection[i + 1];
+
+                    length += distances[currentCity, nextCity];
+                }
+            }
+            else
+                length = 100; // Length if we have duplicate cities is a big number.
+
+            return length;
+        }
+    }
+}
---
+++ TSP_geneticAlg/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP_geneticAlg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Table of distances:
+
+               0A  1B  2C  3D  4E
+           0A   0   4   4   7   3
+           1B   4   0   2   3   5
+           2C   4   2   0   2   3
+           3D   7   3   2   0   6
+           4E   3   5   3   6   0   */
+
+            int populationCount = 16;
+            int repeat = 200;
+            int resultCount = populationCount;
+
+            Console.WriteLine("Running...");
+            Console.WriteLine("(will print top " + resultCount + " results, after " + repeat + " generations)\n");
+
+            // Initialize a population
+            Path[] population = new Path[populationCount];
+            for (int i = 0; i < populationCount; i++) // fill only half of the array, the rest will default to all-A's.
+            {
+                if (i < populationCount / 2)
+                    population[i] = new Path();
+                else
+                    population[i] = new Path(6, 6, 6, 6, 6); // fill the second half with false values, to check for errors.
+            }
+
+            for (int times = 0; times < repeat; times++)
+            {
+                // Create children (using CrossOver)
+                for (int i = 0; i < populationCount / 2; i += 2)
+                {
+                    for (int j = 2; j < 5; j++) // Fill children 1 and 2 with selection 2 to 4 of parents
+                    {
+                        population[populationCount / 2 + i].selection[j] = population[i].selection[j];
+                        population[populationCount / 2 + i + 1].selection[j] = population[i + 1].selection[j];
+                    }
+
+                    bool doubleEntry;
+
+                    //1st child
+                    for (int j = 0; j < 5; j++)
+                    {
+                        doubleEntry = false;
+                        for (int k = 2; k < 5; k++)
+                        {
+                            if (population[i + 1].selection[j] == population[populationCount / 2 + i + 1].selection[k])
+                            {
+                                k = 4;
+                                doubleEntry = true;
+                            }
+                        }
+                        if (!doubleEntry)
+                        {
+                            population[populationCount / 2 + i].selection[0] = population[i + 1].selection[j];
+                            j = 4;
+                        }
+                    }
+                    for (int j = 0; j < 5; j++)
+                    {
+                        doubleEntry = false;
+                        for (int k = 0; k < 5; k++)
+                        {
+                            if (population[i + 1].selection[j] == population[populationCount / 2 + i].selection[k])
+                            {
+                                k = 4;
+                                doubleEntry = true;
+                            }
+                        }
+                        if (!doubleEntry)
+                        {
+                            population[populationCount / 2 + i].selection[1] = population[i + 1].selection[j];
+                            j = 4;
+                        }
+                    }
+                    population[populationCount / 2 + i] = new Path(population[populationCount / 2 + i].selection[0], population[populationCount / 2 + i].selection[1], population[populationCount / 2 + i].selection[2], population[populationCount / 2 + i].selection[3], population[populationCount / 2 + i].selection[4]);
+
+                    //2nd child
+                    for (int j = 0; j < 5; j++)
+                    {
+                        doubleEntry = false;
+                        for (int k = 2; k < 5; k++)
+                        {
+                            if (population[i].selection[j] == population[populationCount / 2 + i + 1].selection[k])
+                            {
+                                k = 4;
+                                doubleEntry = true;
+                            }
+                        }
+                        if (!doubleEntry)
+                        {
+                            population[populationCount / 2 + i + 1].selection[0] = population[i].selection[j];
+                            j = 4;
+                        }
+                    }
+                    for (int j = 0; j < 5; j++)
+                    {
+                        doubleEntry = false;
+                        for (int k = 0; k < 5; k++)
+                        {
+                            if (population[i].selection[j] == population[populationCount / 2 + i + 1].selection[k])
+                            {
+                                k = 4;
+                                doubleEntry = true;
+                            }
+                        }
+                        if (!doubleEntry)
+                        {
+                            population[populationCount / 2 + i + 1].selection[1] = population[i].selection[j];
+                            j = 4;
+                        }
+                    }
+                    population[populationCount / 2 + i + 1] = new Path(population[populationCount / 2 + i + 1].selection[0], population[populationCount / 2 + i + 1].selection[1], population[populationCount / 2 + i + 1].selection[2], population[populationCount / 2 + i + 1].selection[3], population[populationCount / 2 + i + 1].selection[4]);
+                }
+
+                // Sort population
+                Array.Sort(population, delegate (Path path1, Path path2)
+                {
+                    return path1.length.CompareTo(path2.length);
+                });
+
+                // Mutate the two last paths (switch 1st and last cities)
+                for (int i = populationCount - 2; i < populationCount; i++)
+                {
+                    int temp = population[i].selection[0];
+                    population[i].selection[0] = population[i].selection[4];
+                    population[i].selection[4] = temp;
+
+                    population[i] = new Path(population[i].selection[0], population[i].selection[1], 
+                        population[i].selection[2], population[i].selection[3], population[i].selection[4]);
+                }
+
+                // Sort population again
+                Array.Sort(population, delegate (Path path1, Path path2)
+                {
+                    return path1.length.CompareTo(path2.length);
+                });
+            }
+
+            // Results
+            for (int i = 0; i < resultCount; i++)
+            {
+                Console.Write(i + 1 + ". ");
+                for (int j = 0; j < 5; j++)
+                {
+                    switch (population[i].selection[j])
+                    {
+                        case 0:
+                            Console.Write("A");
+                            break;
+                        case 1:
+                            Console.Write("B");
+                            break;
+                        case 2:
+                            Console.Write("C");
+                            break;
+                        case 3:
+                            Console.Write("D");
+                            break;
+                        case 4:
+                            Console.Write("E");
+                            break;
+                    }
+                    //Console.Write(population[i].selection[j]);
+                }
+                Console.WriteLine("\nLength: " + population[i].length + "\n");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            /*// Error Check
+            int hundreds = 0;
+            for(int i=0; i < populationCount; i++)
+            {
+                if (population[i].length == 100)
+                    hundreds++;
+            }
+            Console.WriteLine(hundreds);*/
+        }
+    }
+}
